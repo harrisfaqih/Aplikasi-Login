c8113fe81a3fc60bcbcf99942351cc5f88618422
Harris <101055945+harrisfaqih@users.noreply.github.com>
2023-08-20 09:01:28 +0700

    Update login-script.js
    Menambah space pada setiap baris kode

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -6,20 +6,65 @@
  */
 
 const loginFormElement = document.querySelector('#loginForm');
+
+
+
+
+
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+
+
+
+
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+
+
+
+
+
 const expectedEmail = 'admin@dicoding.com';
-const expectedPassword = 'superpassword';
+
+
+
+
+
+const expectedPassword = 'superpassword'
+
+
+
+
+
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+
+  
+
   const email = inputEmailElement.value;
+
+
+
+
+  
   const password = inputPasswordElement.value;
+
+
+
+
+  
   if (email == expectedEmail && password == expectedPassword) {
+    
     goToHome();
+    
   } else {
+
+    
     showPopUp();
+    
   }
 });
